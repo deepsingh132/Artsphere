095f183593ac506cec03a0acf8f7f8d2de1e6968
Mandeep Singh <mandeeparora132@gmail.com>
2023-11-29 02:34:40 +0530

    fix not found error on inital page load

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,19 @@ export const metadata: Metadata = {
     "Artsphere is a platform for artists to share their work and connect with other artists.",
 };
 
-// console.log("Current environment: ", process.env.NODE_ENV);
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning={true} lang="en">
-      <body suppressHydrationWarning={true}>
-        <ThemeProvider attribute="class">
-          <RecoilContextProvider>
-            <NextAuthProvider>{children}</NextAuthProvider>
-          </RecoilContextProvider>
-        </ThemeProvider>
+    <html suppressHydrationWarning lang="en">
+      <body suppressHydrationWarning>
+        <RecoilContextProvider>
+          <NextAuthProvider>
+            <ThemeProvider attribute="class">{children}</ThemeProvider>
+          </NextAuthProvider>
+        </RecoilContextProvider>
       </body>
     </html>
   );
