df6b39a5c65dcd5836a65617aec1a08a3a44c084
Mandeep Singh <mandeeparora132@gmail.com>
2024-06-19 18:32:56 +0530

    Fixes jsdom css parsing console errors when running tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,23 @@
-import '@testing-library/jest-dom'
-import 'whatwg-fetch'
-import { server } from './__mocks__/server'
+import "@testing-library/jest-dom";
+import "whatwg-fetch";
+import { server } from "./__mocks__/server";
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+beforeAll(() => {
+  // Disable error logs from jsdom css parsing error
+  /**
+   * This is a workaround for the following issue:
+   * NOTE: The error message "Error: Could not parse CSS stylesheet" is thrown by jsdom when it tries to parse a CSS file that contains an unsupported CSS feature.
+   * TODO: Remove this workaround once the issue is fixed in jsdom.
+   */
+  const originalConsoleError = console.error;
+  const jsDomCssError = "Error: Could not parse CSS stylesheet";
+  console.error = (...params) => {
+    if (!params.find((p) => p.toString().includes(jsDomCssError))) {
+      originalConsoleError(...params);
+    }
+  };
+
+  server.listen();
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
